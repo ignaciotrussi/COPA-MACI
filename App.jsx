b8007b08b01a8cbf1de501a68dbbf9d19379c013
ignaciotrussi <ignaciotrussi@gmail.com>
2026-03-26 18:24:41 -0300

    Update App.jsx

--- App.jsx
+++ App.jsx
@@ -1,7 +1,11 @@
 import React from 'react'
-import ReactDOM from 'react-dom/client'
-import App from './App.jsx'
 
-ReactDOM.createRoot(document.getElementById('root')).render(
-  <React.StrictMode><App /></React.StrictMode>
-)
+function App() {
+  return (
+    <div>
+      <h1>Copa Maci - App Funcionando</h1>
+    </div>
+  )
+}
+
+export default App
